1f036b75b9baee1711268e5bb0be6e15be909b40
Perty53 <alexandrepertiny693@gmail.com>
2022-08-09 15:35:41 -0400

    Update

---
+++ js/ajaxUtils.js
@@ -0,0 +1,29 @@
+(function(main){
+    const request = new XMLHttpRequest();
+    var ajaxUtils = {};
+
+    ajaxUtils.sendGetRequest = function(requestUrl, responseHandler, isJsonReponse){
+        request.onreadystatechange = function(){
+            handleResponse(requestUrl, responseHandler, isJsonReponse);
+        }
+
+        request.open("GET", requestUrl, true);
+        request.send(null);
+    };
+
+    function handleResponse(requestUrl, responseHandler, isJsonReponse){
+        if(request.readyState == 4 && request.status == 200){
+            if(isJsonReponse == undefined){
+                isJsonReponse == true;
+            }
+
+            if(isJsonReponse){
+                responseHandler(JSON.parse(request.responseText));
+            } else {
+                responseHandler(request.responseText);
+            }
+        }
+    }
+
+    main.$ajaxUtils = ajaxUtils;
+})(window)
--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 $(function(){
-    $("#navbarToggle").blur(function(event){
-        var scWidth = window.innerWidth;
-        if(scWidth < 768){
-            $("#navbarSupportedContent").collapse("hide");
-            console.log("ASD");
-        }
-    });
+    $("#navbarToggle").blur(
+        function(){
+            var scWidth = window.innerWidth;
+            if(scWidth < 768){
+                $("#navbarSupportedContent").collapse("hide");
+            }
+        });
 });
+
+(function(global){
+    var dc = {};
+
+    var homeHtml = "snippets/home.html";
+
+    var insertHtml = function(selector, html){
+        var targetElement = document.querySelector(selector);
+        targetElement.innerHTML = html; 
+    }
+
+    var showLoading = function(selector){
+        var html = "<div class='text-center'><img src='images/loading.gif'></div>";
+        insertHtml(selector, html);
+    }
+
+    document.addEventListener("DOMContentLoaded", function(event){
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(homeHtml, function(responseText){
+            document.querySelector("#main-content").innerHTML = responseText;
+        }, false);
+    });
+
+    global.$dc = dc;
+})(window);
